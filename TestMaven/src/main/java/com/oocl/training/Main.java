a59d29b1646429dc5d559d9ada120ba110f1b997
Janiel Bariso <Janiel Bariso@10.0.1.14>
2017-08-07 00:27:21 +0800

    Exercises 10c and 10b

--- TestMaven/src/main/java/com/oocl/training/Main.java
+++ TestMaven/src/main/java/com/oocl/training/Main.java
@@ -1,16 +1,57 @@
 package com.oocl.training;
 
+import java.util.ArrayList;
+
 public class Main {
-	
 
-	static void fib(int x, int y) {
+	public ArrayList<String> exer10c() {
+		int value = 5;
+		int value2 = 1;
+		ArrayList<String> out = new ArrayList<String>();
 
-		if (y > 100) {
-			return;
+		while (value2 < 4) {
+			for (; value >= value2; value--) {
+				out.add("*");
+			}
+			value2++;
 		}
-		System.out.print(y + " ");
+		return out;
+	}
 
-		fib(y, y + x);
+	static int value = 10;
 
+	public static void main(String[] args) {
+		
+		int i = exer10b();
+		System.out.println(i);
+	}
+	
+	public static int exer10b() {
+		System.out.println("Main before p1: " + value);
+		value = p1(value);
+		System.out.println("Main before p2: " + value);
+		value = p2(value);
+		System.out.println("Main at the end: " + value);
+		System.out.println("-----");
+		int value = 30;
+		System.out.println("Main before p1: " + value);
+		value = p1(value);
+		System.out.println("Main before p2: " + value);
+		value = p2(value);
+		System.out.println("Main at the end: " + value);
+		return value;
+	}
+
+	static int p1(int input) {
+		value = input * value;
+		System.out.println("In p1: " + value);
+		return value;
+	}
+
+	static int p2(int input) {
+		int value = 5;
+		value = input / value;
+		System.out.println("In p2: " + value);
+		return value;
 	}
 }
--- TestMaven/src/test/java/com/oocl/training/App.java
+++ TestMaven/src/test/java/com/oocl/training/App.java
@@ -2,23 +2,33 @@ package com.oocl.training;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.ArrayList;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 public class App {
 
+	@Test
+	public void testExer10c() {
+		Main m = new Main();
 
+		ArrayList<String> act = m.exer10c();
+
+		assertEquals(m.exer10c(), act);
+
+	}
 
 	@Test
-	public void testFib() {
-		Main fib = new Main();
-		fib.fib(0, 1);
-		
+	public void testExer10b() {
+		Main m = new Main();
+
+		int act = m.exer10b();
+
+		assertEquals(120, act);
+
 	}
-	
 	public void test() {
-		//fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
